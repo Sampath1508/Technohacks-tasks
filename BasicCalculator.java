45b79415b9bdeabb4cdba63a3fe6a03a2b37f12a
Sampath1508 <134853532+Sampath1508@users.noreply.github.com>
2023-07-09 08:12:16 +0530

    BasicCalculator.java

---
+++ BasicCalculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        
+        System.out.println("Select an operation (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        
+        double result = 0.0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                    System.out.println("Error: Division by zero is not allowed.");
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+        
+        System.out.println("Result: " + result);
+    }
+}
